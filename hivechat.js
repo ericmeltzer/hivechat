88c403486a8b3dc80559ed7d49fe56c2bce7c0f2
Eric <eric.meltzer@gmail.com>
2011-03-16 20:52:08 +0800

    second

---
+++ hivechat.js
@@ -0,0 +1,14 @@
+﻿var fs = require('fs');
+var server = require('http').createServer(function(req, response){
+  fs.readFile('template.html', function(err, data){
+    response.writeHead(200, {'Content-Type':'text/html'});  
+    response.write(data);  
+    response.end();
+  });
+});
+server.listen(8080);
+var everyone = require("../lib/nowServerLib.js").initialize(server);
+
+
+
+everyone.now.distributeMessage = function(message){everyone.now.receiveMessage(message);};
